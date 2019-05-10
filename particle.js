93b700ed9f2c8c0ef329df893f12fcec1dea6594
Robert <rob.louis@gmail.com>
2019-05-10 10:20:53 -0800

    starting 3d mode

--- particle.js
+++ particle.js
@@ -2,12 +2,13 @@ class Particle {
     constructor() {
         this.pos = createVector(width / 2, height / 2);
         this.rays = [];
-        for (let i = 0; i < 360; i += 2) {
+        for (let i = 180; i < 270; i += 1) {
             this.rays.push(new Ray(this.pos, radians(i)));
         }
     }
 
     look(wall) {
+        const scene = [];
         for (let ray of this.rays) {
             let closest = null;
             let record = Infinity;
@@ -22,9 +23,12 @@ class Particle {
                 }
             }
             if(closest) {
+                stroke(255, 100);
                 line(this.pos.x, this.pos.y, closest.x, closest.y);
             }
+                scene.push(record);
         }
+        return scene
     }
 
     update(x, y) {
--- sketch.js
+++ sketch.js
@@ -2,24 +2,27 @@ let walls = [];
 let ray;
 let particle;
 
+const sceneW = 400;
+const sceneH = 400;
+
 function setup() {
-	createCanvas(600, 550);
-    walls.push(new Boundary(0,0, width, 0));
-    walls.push(new Boundary(0,0, 0, height));
-    walls.push(new Boundary(0,height, width, height));
-    walls.push(new Boundary(width,0, width, height));
+	createCanvas(800, 400);
+    walls.push(new Boundary(0,0, sceneW, 0));
+    walls.push(new Boundary(0,0, 0, sceneH));
+    walls.push(new Boundary(0,sceneH, sceneW, sceneH));
+    walls.push(new Boundary(sceneW,0, sceneW, sceneH));
     for (i = 0; i < 5; i++) {
-        let x1 = random(width);
-        let x2 = random(width);
-        let y1 = random(height);
-        let y2 = random(height);
+        let x1 = random(sceneW);
+        let x2 = random(sceneW);
+        let y1 = random(sceneH);
+        let y2 = random(sceneH);
         walls.push(new Boundary(x1, y1, x2, y2));
-        // walls.push(new Boundary(random(width), random(height), random(width), random(height)));
-        // walls.push(new Boundary(random(width), random(height), random(width), random(height)));
-        // walls.push(new Boundary(random(width), random(height), random(width), random(height)));
-        // walls.push(new Boundary(random(width), random(height), random(width), random(height)));
-        // walls.push(new Boundary(random(width), random(height), random(width), random(height)));
-        // walls.push(new Boundary(random(width), random(height), random(width), random(height)));
+        // walls.push(new Boundary(random(sceneW), random(sceneH), random(sceneW), random(sceneH)));
+        // walls.push(new Boundary(random(sceneW), random(sceneH), random(sceneW), random(sceneH)));
+        // walls.push(new Boundary(random(sceneW), random(sceneH), random(sceneW), random(sceneH)));
+        // walls.push(new Boundary(random(sceneW), random(sceneH), random(sceneW), random(sceneH)));
+        // walls.push(new Boundary(random(sceneW), random(sceneH), random(sceneW), random(sceneH)));
+        // walls.push(new Boundary(random(sceneW), random(sceneH), random(sceneW), random(sceneH)));
     }
     particle = new Particle();
 }
@@ -31,7 +34,20 @@ function draw() {
         wall.update();
         wall.show();
     }
-    particle.look(walls);
+    const scene = particle.look(walls);
+
+    const w = sceneW / scene.length;
+    push();
+    translate(sceneW,0);
+    for (let i = 0; i < scene.length; i ++) {
+        noStroke();
+        const h = map(scene[i], 0, sceneW, sceneH, 0);
+        fill(255-scene[i]);
+        rectMode(CENTER);
+        rect(i*w + w / 2, sceneH / 2, w, h);
+
+    }
+    pop();
     particle.update(mouseX, mouseY);
     particle.show();
 
